f93c9ac3b5de2258fc2e57fe2eb1e8eb6f756235
Amit Hakmon <137507045+AmitH-15@users.noreply.github.com>
2024-03-24 11:56:04 +0200

    Update lottieInteractivityTry.js

--- lottieInteractivityTry.js
+++ lottieInteractivityTry.js
@@ -1,16 +1,20 @@
-console.log('lottieInteractivityTry')
+init()
 
-document.addEventListener("load", function() {
-    console.log('load')
-    LottieInteractivity.create({
-    player: '#lottie-amit',
-    mode: 'scroll',
-    actions: [
-      {
-        visibility: [0,1],
-        type: 'seek',
-        frames: [0, 300],
-      },
-    ]
-  });
-});
+function init() {
+    console.log('lottieInteractivityTry')
+
+    document.addEventListener("load", function() {
+        console.log('load')
+        LottieInteractivity.create({
+        player: '#lottie-amit',
+        mode: 'scroll',
+        actions: [
+          {
+            visibility: [0,1],
+            type: 'seek',
+            frames: [0, 300],
+          },
+        ]
+      });
+    });
+}
